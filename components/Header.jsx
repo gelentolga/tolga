9b296588cc512eb6606faf4ebce3c2c98bbae1da
gelentolga <gelentolga@gmail.com>
2023-07-23 12:36:02 +1000

    color

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
             <div>
               <div className="flex items-center justify-between py-1 md:block">
                 <Link
-                  className="text-3xl hover:tracking-tight tracking-tighter font-bold max-md:text-xl text-[#ffffff9c] font-Poppins"
+                  className="text-3xl hover:tracking-tight tracking-tighter font-bold max-md:text-xl text-[#dedede9c] font-Poppins"
                   href="/"
                 >
                   TOLGA
